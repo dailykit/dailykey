0a495874a3e2e28e45307d42be26c01f77d102ed
Praveen Bisht <prvnbist@gmail.com>
2021-07-20 19:01:49 +0530

    feat: create stripe customer from datahub event

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import {
    PaymentIntentRouter,
    PaymentRouter,
    createStripeCustomer,
+   createStripeCustomer2,
    sendStripeInvoice,
    sendSMS,
 } from './entities'
@@ -71,6 +72,7 @@ app.post('/api/webhooks/authorize-request', authorizeRequest)
 app.post('/api/webhooks/customer-by-client', createCustomerByClient)
 app.post('/api/webhooks/dailyos-stripe-status', updateDailyosStripeStatus)
 app.post('/api/webhooks/stripe/customer', createStripeCustomer)
+app.post('/api/webhooks/stripe/customer/create', createStripeCustomer2)
 app.post('/api/webhooks/stripe/send-invoice', sendStripeInvoice)
 app.post('/api/webhooks/stripe/send-sms', sendSMS)
 
